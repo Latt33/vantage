9b84476eba94b0b67208ce1c0485a23f3ea4587f
Latt33 <atte.laakso@aalto.fi>
2026-05-15 22:54:40 +0300

    reset

--- apps/web/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ['@hackathon/shared', '@hackathon/db'],
-  output: 'standalone',
-};
-
-module.exports = nextConfig;
